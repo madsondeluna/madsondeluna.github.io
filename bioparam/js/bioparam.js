4c673392204322afb798b1c80b148d32d24ebdda
madsondeluna <15664591+madsondeluna@users.noreply.github.com>
2025-09-04 22:07:20 -0300

    Update comment to clarify code context after beta phase

--- bioparam/js/bioparam.js
+++ bioparam/js/bioparam.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
         type();
     }
     
-    // --- O RESTO DO CÓDIGO PERMANECE O MESMO ---
+    // O RESTO DO CÓDIGO PERMANECE O MESMO, APÓS SAIR DO BETA
     const molecularWeights = { 'A': 89.09, 'R': 174.20, 'N': 132.12, 'D': 133.10, 'C': 121.16, 'E': 147.13, 'Q': 146.15, 'G': 75.07, 'H': 155.16, 'I': 131.17, 'L': 131.17, 'K': 146.19, 'M': 149.21, 'F': 165.19, 'P': 115.13, 'S': 105.09, 'T': 119.12, 'W': 204.23, 'Y': 181.19, 'V': 117.15 };
     const pKa = { 'N_term': 7.5, 'K': 10.5, 'R': 12.5, 'H': 6.5, 'D': 3.9, 'E': 4.3, 'C': 8.5, 'Y': 10.1, 'C_term': 3.5 };
     const kyteDoolittle = { 'A': 1.8, 'R': -4.5, 'N': -3.5, 'D': -3.5, 'C': 2.5, 'E': -3.5, 'Q': -3.5, 'G': -0.4, 'H': -3.2, 'I': 4.5, 'L': 3.8, 'K': -3.9, 'M': 1.9, 'F': 2.8, 'P': -1.6, 'S': -0.8, 'T': -0.7, 'W': -0.9, 'Y': -1.3, 'V': 4.2 };
